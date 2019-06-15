aab9234a6abceb47ffe075d8d8e09f44a8f0038f
Zhao, Hancheng <hzhao@sevone.com>
2019-06-14 23:57:22 -0400

    add printall function

--- SkipList/skipList.py
+++ SkipList/skipList.py
@@ -15,7 +15,7 @@ class Node:
         # if the level of this node is l
         # forward[l] is the next node on this level
         # forward[l-n] is the nth node below this node
-        self.forwards = [None * MAX_LEVEL]
+        self.forwards = [None] * MAX_LEVEL
 
 
 class SkipList:
@@ -53,7 +53,7 @@ class SkipList:
     def insert(self, value):
         level = self.randomLevel()
         # record the update node on each level
-        update = [None * level]
+        update = [None] * level
         newNode = Node(value)
         p = self.head
         for i in range(level - 1, -1, -1):
@@ -69,14 +69,42 @@ class SkipList:
     # remove a value
     def remove(self, value):
         p = self.head
-        update = [None * self.levelCount]
+        update = [None] * self.levelCount
         # find the maximum node whose value is smaller than value
         for i in range(self.levelCount - 1, -1, -1):
             while p.forwards[i] and p.forwards[i].data < value:
                 p = p.forwards[i]
             update[i] = p
         if p.forwards[0] and p.forwards[0].data == value:  # found this value
-            for i in range(self.levelCount, -1, -1):
+            for i in range(self.levelCount - 1, -1, -1):
                 if update[i].forwards[i] and update[i].forwards[i].data == value:
                     # link to next node on the same level
-                    update[i].forwards[i] = update[i].forward[i].forwards[i]
+                    update[i].forwards[i] = update[i].forwards[i].forwards[i]
+
+    # print all nodes
+    def printAll(self):
+        heads = [None] * self.levelCount
+        print("Level count is {}".format(self.levelCount))
+        # get the first node on each level
+        for i in range(self.levelCount - 1, -1, -1):
+            heads[i] = self.head.forwards[i]
+        for i in range(self.levelCount - 1, -1, -1):
+            p = heads[i]
+            while p and p.forwards[i]:
+                print("{} -> ".format(p.data), end=" ")
+                p = p.forwards[i]
+            if p:
+                print(str(p.data))
+
+
+if __name__ == "__main__":
+    sl = SkipList()
+    for i in range(1, 10):
+        sl.insert(i)
+    sl.printAll()
+    sl.remove(2)
+    sl.printAll()
+    sl.remove(1)
+    sl.printAll()
+    sl.remove(9)
+    sl.printAll()
